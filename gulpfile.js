e825a14a147b45811d65b990123bde647bcf73d0
Paulo Trajano <paulo@rallyhr.com>
2017-10-08 15:54:56 +0800

    Initial Eversafe Layout Implementation

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('default', function() {
+  gulp.src('css/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('watch',function() {
+  gulp.watch('css/**/*.scss', ['default']);
+});
